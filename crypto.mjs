9a675eae37909b32f58fa41577bff8c8ef28cacc
matheustanaka <matheustanakanog@gmail.com>
2022-05-17 13:18:27 -0300

    fix: rename variables and fix wrong comments

--- crypto.mjs
+++ crypto.mjs
@@ -3,19 +3,18 @@ import fetch from "node-fetch";
 import yargs from "yargs";
 
 const { argv } = yargs(process.argv);
-
 const response = await fetch("https://api2.binance.com/api/v3/ticker/24hr");
 const data = await response.json();
 
-let largestLastPrice = "";
+let largestPrice = "";
 let symbol = "";
 let priceChangePercent = "";
 let highPrice = "";
 let dollarUSLocale = Intl.NumberFormat("en-US");
 
 data.forEach((crypto) => {
-  if (crypto.lastPrice > largestLastPrice) {
-    largestLastPrice = crypto.lastPrice;
+  if (crypto.lastPrice > largestPrice) {
+    largestPrice = crypto.lastPrice;
     symbol = crypto.symbol;
     priceChangePercent = crypto.priceChangePercent;
     highPrice = crypto.highPrice;
@@ -26,18 +25,18 @@ data.forEach((crypto) => {
 if (argv.price) {
     console.log(`
     The largest Crypto Coin Price in the last 24 Hours\n
-    Price: $${dollarUSLocale.format(largestLastPrice)}\n
+    Price: $${dollarUSLocale.format(largestPrice)}\n
     Symbol: ${symbol}\n
     Price Change Percent: %${priceChangePercent}\n
     High Price: $${dollarUSLocale.format(highPrice)}
     `);
 }
-// Commands CLI: crypto --ethereum
+// Commands CLI: crypto-status --ethereum
 if (argv.ethereum) {
     data.filter((crypto) => { 
         if(crypto.symbol == "ETHUSDT") {
             console.log(`
-            Ethereum Avarage Price in the last 24 Hours\n
+            Ethereum Average Price in the last 24 Hours\n
             Symbol: ${crypto.symbol}\n
             Price: $${dollarUSLocale.format(crypto.lastPrice)}\n
             Price Change Percent: %${crypto.priceChangePercent}\n
@@ -45,12 +44,12 @@ if (argv.ethereum) {
         }
     })
 }
-// Commands CLI: crypto --bitcoin
+// Commands CLI: crypto-status --bitcoin
 if (argv.bitcoin) {
     data.filter((crypto) => { 
         if(crypto.symbol == "BTCUSDT") {
             console.log(`
-            Bitcoin Avarage Price in the last 24 Hours\n
+            Bitcoin Average Price in the last 24 Hours\n
             Symbol: ${crypto.symbol}\n
             Price: $${dollarUSLocale.format(crypto.lastPrice)}\n
             Price Change Percent: %${crypto.priceChangePercent}\n
