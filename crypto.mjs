aeb65a57a4816998c504a9940ad50251c3a672c3
matheustanaka <matheustanakanog@gmail.com>
2022-05-24 09:21:54 -0300

    fix: code format

--- crypto.mjs
+++ crypto.mjs
@@ -12,48 +12,50 @@ let priceChangePercent = "";
 let highPrice = "";
 let dollarUSLocale = Intl.NumberFormat("en-US");
 
-data.forEach((crypto) => {
-  if (crypto.lastPrice > largestPrice) {
-    largestPrice = crypto.lastPrice;
-    symbol = crypto.symbol;
-    priceChangePercent = crypto.priceChangePercent;
-    highPrice = crypto.highPrice;
-  }
-});
-
 // Commands CLI: crypto --price
 if (argv.price) {
-    console.log(`
-    The largest Crypto Coin Price in the last 24 Hours\n
-    Price: $${dollarUSLocale.format(largestPrice)}\n
-    Symbol: ${symbol}\n
-    Price Change Percent: %${priceChangePercent}\n
-    High Price: $${dollarUSLocale.format(highPrice)}
+  data.forEach((crypto) => {
+    if (crypto.lastPrice > largestPrice) {
+      largestPrice = crypto.lastPrice;
+      symbol = crypto.symbol;
+      priceChangePercent = crypto.priceChangePercent;
+      highPrice = crypto.highPrice;
+    }
+  });
+
+  console.log(`
+        The largest Crypto Coin Price in the last 24 Hours\n
+        Price: $${dollarUSLocale.format(largestPrice)}\n
+        Symbol: ${symbol}\n
+        Price Change Percent: %${priceChangePercent}\n
+        High Price: $${dollarUSLocale.format(highPrice)}
     `);
 }
 // Commands CLI: crypto-status --ethereum
 if (argv.ethereum) {
-    data.filter((crypto) => { 
-        if(crypto.symbol == "ETHUSDT") {
-            console.log(`
-            Ethereum Average Price in the last 24 Hours\n
-            Symbol: ${crypto.symbol}\n
-            Price: $${dollarUSLocale.format(crypto.lastPrice)}\n
-            Price Change Percent: %${crypto.priceChangePercent}\n
-            High Price: $${dollarUSLocale.format(crypto.highPrice)}`)
-        }
-    })
+  data.filter((crypto) => {
+    if (crypto.symbol == "ETHUSDT") {
+      console.log(`
+        Ethereum Average Price in the last 24 Hours\n
+        Symbol: ${crypto.symbol}\n
+        Price: $${dollarUSLocale.format(crypto.lastPrice)}\n
+        Price Change Percent: %${crypto.priceChangePercent}\n
+        High Price: $${dollarUSLocale.format(crypto.highPrice)}\n
+        `);
+    }
+  });
 }
 // Commands CLI: crypto-status --bitcoin
 if (argv.bitcoin) {
-    data.filter((crypto) => { 
-        if(crypto.symbol == "BTCUSDT") {
-            console.log(`
-            Bitcoin Average Price in the last 24 Hours\n
-            Symbol: ${crypto.symbol}\n
-            Price: $${dollarUSLocale.format(crypto.lastPrice)}\n
-            Price Change Percent: %${crypto.priceChangePercent}\n
-            High Price: $${dollarUSLocale.format(crypto.highPrice)}`)
-        }
-    })    
+  data.filter((crypto) => {
+    if (crypto.symbol == "BTCUSDT") {
+      console.log(`
+        Bitcoin Average Price in the last 24 Hours\n
+        Symbol: ${crypto.symbol}\n
+        Price: $${dollarUSLocale.format(crypto.lastPrice)}\n
+        Price Change Percent: %${crypto.priceChangePercent}\n
+        High Price: $${dollarUSLocale.format(crypto.highPrice)}\n
+        `);
+    }
+  });
 }
